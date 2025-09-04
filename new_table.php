52cab9c41ffe4c618b4c8c0f4620b5c8311e2301
MoTyaZ1 <100450367+MoTyaZ1@users.noreply.github.com>
2025-09-04 12:19:25 +0300

    Delete new_table.php

--- new_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=db01','root','');
-
-$pdo->exec("CREATE TABLE users (
-            id INT,
-            login VARCHAR(50),
-            pass VARCHAR(50))");
-?>
