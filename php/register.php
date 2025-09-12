4607385b1f9d9882ec519e07cfc0e7273cb143dc
MoTyaZ <100450367+MoTyaZ1@users.noreply.github.com>
2025-09-12 10:27:06 +0300

    Delete php/register.php

--- php/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once('db.php');
-$email = $_POST['email'];
-$pass = $_POST['pass'];
-
-$sql = "INSERT INTO `database` (email,pass) VALUES ('$email', '$pass')";
-
-$conn -> query($sql);
